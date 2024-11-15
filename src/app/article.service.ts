e67bbf48488eaaf6332dca201ef264c3052d6b58
CoolTrainerEX <92789937+CoolTrainerEX@users.noreply.github.com>
2024-11-15 11:30:56 +0800

    home links

--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -6,19 +6,22 @@ import { Injectable } from '@angular/core';
 export class ArticleService {
   articles = [
     {
+      id: '1',
       title: 'yes',
       author: 'no',
       img: 'https://upload.wikimedia.org/wikipedia/commons/5/56/Donald_Trump_official_portrait.jpg',
       content: 'yes',
     },
     {
+      id: '2',
       title: 'no',
       author: 'yes',
       img: 'https://upload.wikimedia.org/wikipedia/commons/6/68/Joe_Biden_presidential_portrait.jpg',
       content: 'yes',
     },
     {
-      title: 'yes',
+      id: '3',
+      title: 'ye s',
       author: 'yes',
       img: 'https://upload.wikimedia.org/wikipedia/commons/8/8d/President_Barack_Obama.jpg',
       content: 'yes',
