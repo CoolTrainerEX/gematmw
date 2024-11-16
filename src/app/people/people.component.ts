58317e6beebda3415d2710a27bc41b5e4cbc6820
CoolTrainerEX <92789937+CoolTrainerEX@users.noreply.github.com>
2024-11-16 14:00:37 +0800

    people

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-people',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './people.component.html',
   styleUrl: './people.component.scss',
 })
-export class PeopleComponent {}
+export class PeopleComponent {
+  faEnvelope = faEnvelope;
+  faFacebook = faFacebook;
+}
